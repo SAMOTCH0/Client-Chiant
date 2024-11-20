ccce9d19770e0c5b4cf08d450313488a90b4c078
Thomas METHIVIER <59664905+SAMOTCH0@users.noreply.github.com>
2024-11-20 10:53:38 +0000

    ezef

--- test.php
+++ test.php
@@ -3,11 +3,14 @@ require_once 'ConnexionBDD.php';
 $test = PDOsql::connexion();
 
 $user = $_GET['user'];
+$prenom = "TG";
+$email = "TG";
 
 
-
-$requete = $test->prepare("insert into Utilisateur (Nom) values (:user)");
+$requete = $test->prepare("insert into Utilisateur (Nom,Prenom,Email) values (:user , :prenom , :email)");
 $requete->bindParam(':user', $user);
+$requete->bindParam(':prenom', $prenom);
+$requete->bindParam(':email', $email);
 $requete->execute();
 $result = $requete->fetchAll(PDO::FETCH_ASSOC);
 
